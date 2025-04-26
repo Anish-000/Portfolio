f9d67759219b4193b448b06f545400e8e5e98853
Anish Chattopadhyay <165559685+Anish-000@users.noreply.github.com>
2025-04-26 05:46:43 +0530

    Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,8 @@ import Thal from '../../assets/images/Thal.jpg';
 import Donut from '../../assets/images/Donut.jpg';
 import Quantum from '../../assets/images/Quantum.jpg';
 import Portfolio from '../../assets/images/portfolio.jpg';
+import reactjs from '../../assets/images/Certificate-react.jpg';
+import thalc from '../../assets/images/Certificate-thal.jpg';
 
 const Projects = () => {
   const [filter, setFilter] = useState('all');
@@ -30,7 +32,7 @@ const Projects = () => {
       title: "Donut Website Frontend",
       category: "web",
       image: Donut,
-      description: "A responsive donut frontend website",
+      description: "A fully responsive Frontend Donut Website built using HTML, CSS, and JavaScript.The website provides user-friendly design, showcasing various donut varieties with interactive features.",
       technologies: ["HTML5", "CSS3","JavaScript"],
       github: "https://github.com/Anish-000/Donut-Frontend",
       live: "https://project3.com"
@@ -39,10 +41,26 @@ const Projects = () => {
       title: "Portfolio Website",
       category: "web",
       image: Portfolio,
-      description: "Personal portfolio website showcasing projects and skills.",
+      description: "Personal portfolio website showcasing projects and skills. I used ReactJS to create the portfolio.",
       technologies: ["React", "CSS3", "AOS"],
       github: "https://github.com/Anish-000/Portfolio",
       live: "https://project4.com"
+    },
+    {
+      title: "React JS Certificate",
+      category: "certificates",
+      image: reactjs,
+      description: "Successfully completed the React JS for Beginners course offered by Simplilearn.This course provided a foundational understanding of key concepts such as components, JSX, props, and state management. ",
+      technologies: ["React Component", "React Props", "React State"],
+      github: "https://github.com/Anish-000/Portfolio/blob/main/src/assets/images/Certificate-react.jpg",
+    },
+    {
+      title: "Thalassemia Screening Device Certificate",
+      category: "certificates",
+      image: thalc,
+      description: "I obtained the Thalassemia Screening Device Certificate by developing the device named Thalassemia Screening Device which is my firts official work. ",
+      technologies: ["Arduino Uno", "LCD", "Keyboard", "C++"],
+      github: "https://github.com/Anish-000/Portfolio/blob/main/src/assets/images/Certificate-thal.jpg",
     }
   ];
 
