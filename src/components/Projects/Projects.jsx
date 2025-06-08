7b702f34f2eee69506fe1ca061ac0c5c5158abec
Anish Chattopadhyay <165559685+Anish-000@users.noreply.github.com>
2025-06-08 09:14:39 +0530

    Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,7 @@ import Portfolio from '../../assets/images/portfolio.jpg';
 import reactjs from '../../assets/images/Certificate-react.jpg';
 import thalc from '../../assets/images/Certificate-thal.jpg';
 import cloudc from '../../assets/images/Cloud-certificate.jpg';
+import cake from '../../assets/images/Cake.jpg';
 
 const Projects = () => {
   const [filter, setFilter] = useState('all');
@@ -36,7 +37,16 @@ const Projects = () => {
       description: "A fully responsive Frontend Donut Website built using HTML, CSS, and JavaScript.The website provides user-friendly design, showcasing various donut varieties with interactive features.",
       technologies: ["HTML5", "CSS3","JavaScript"],
       github: "https://github.com/Anish-000/Donut-Frontend",
-      live: "https://project3.com"
+      live: "https://donut-frontend.vercel.app/"
+    },
+    {
+      title: "Cake Website Frontend",
+      category: "web",
+      image: cake,
+      description: "A fully responsive Frontend Cake Website built using React JS and CSS.The website provides user-friendly design, showcasing various cakes varieties with interactive features.",
+      technologies: ["React JS", "CSS3"],
+      github: "https://github.com/Anish-000/Sweet-Delights",
+      live: "https://sweet-delights-wheat.vercel.app/"
     },
     {
       title: "Portfolio Website",
@@ -45,7 +55,7 @@ const Projects = () => {
       description: "Personal portfolio website showcasing projects and skills. I used ReactJS to create the portfolio.",
       technologies: ["React", "CSS3", "AOS"],
       github: "https://github.com/Anish-000/Portfolio",
-      live: "https://project4.com"
+      live: "https://anish-portfolio-plum.vercel.app/"
     },
     {
       title: "React JS Certificate",
